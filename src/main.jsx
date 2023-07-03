31757c127d3339110613465b0428cf55f4ed8141
karolymUXE <kmontiel@buk.cl>
2023-07-02 21:41:30 -0400

    feat: create ci with netlify

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import '@styles/main.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { AuthContextProvider } from "./context/authContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <App />
   </React.StrictMode>
 );
